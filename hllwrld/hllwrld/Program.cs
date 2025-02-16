27899174ebdc1cc22bae28a4f8d893849dbd04af
waytenshi <ddiimmaa567@gmail.com>
2025-02-16 21:27:05 +0300

    Second project

---
+++ hllwrld/hllwrld/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("eshkere");
+            Console.ReadKey();
+        }
+    }
+}
+
